f7c1029370b58b2236d26271e0e918e0cdb06ed5
Kevin Wayne <kwayne11@gmail.com>
2016-02-06 13:54:47 -0500

    Master layout created

---
+++ resources/views/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>PeeDee</title>
+    @section('headLinks')
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel = "stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
+    @show
+</head>
+
+<body>
+    @include('menubar.menubar')
+<div>
+    @yield('content')
+</div>
+
+</body>
+
+</html>
