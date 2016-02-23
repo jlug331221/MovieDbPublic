4509f02378a601940eeaee856fa0b73d480a011f
dixonj13 <dixonj13@students.ecu.edu>
2016-02-23 04:52:52 -0500

    added images table

---
+++ database/migrations/2016_02_23_055302_create_images_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('images', function (Blueprint $table) {
+            $table->string('name')->unique();
+            $table->string('path');
+            $table->string('extension', 10);
+            $table->timestamps();
+        });
+
+        // add the id column as a 16-bit binary field
+        DB::statement('
+            ALTER TABLE `images`
+            ADD `id` BINARY(16) FIRST;
+        ');
+
+        // make the id column the primary key
+        DB::statement('
+            ALTER TABLE `images`
+            ADD PRIMARY KEY (`id`);
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('images');
+    }
+}
