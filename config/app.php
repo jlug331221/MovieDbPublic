7bc04fe43845be0318c6cc3cfa4ecdba620eace5
parksm13 <parksm13@students.ecu.edu>
2016-02-25 12:44:23 -0500

    Added laravelcollective/html service provider and aliases

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Intervention\Image\ImageServiceProvider::class,
+	Collective\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -204,6 +205,8 @@ return [
         'View'      => Illuminate\Support\Facades\View::class,
         'Image'     => Intervention\Image\Facades\Image::class,
         'Uuid'      => Webpatser\Uuid\Uuid::class,
+	'Form'	    => Collective\Html\FormFacade::class,
+	'Html'	    => Collective\Html\HtmlFacade::class,
 
     ],
 
