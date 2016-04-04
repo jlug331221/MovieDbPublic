d4eb6fcd5202f63e8401f579fbeef05361a6c952
parksm13 <parksm13@students.ecu.edu>
2016-04-04 16:17:48 -0400

    Added 'table' css to searchResultsWidget

--- resources/views/search/searchResultsWidget.blade.php
+++ resources/views/search/searchResultsWidget.blade.php
@@ -4,18 +4,18 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Search Results</div>
                 <div class="panel-body">
-                    <table class="list-group">
-		        <tr class="list-group-item">
+                    <table class="table table-hover">
+		        <tr>
                             <td>Title</td>
                             <td>Release Date</td>
                             <td>Runtime</td>
                             <td>Genre</td>
                         </tr>
                         @foreach($movies as $movie)
-			    <tr class="list-group-item">
+			    <tr>
                                 <td>{!! $movie->title !!}</td>
                                 <td>{!! $movie->release_date !!}</td>
-				<td>{!! $movie->runtime !!}</td>
+				<td>{!! $movie->runtime !!} min</td>
 				<td>{!! $movie->genre !!}</td>
                             </tr>
                         @endforeach
