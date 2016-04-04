2dc84590e21cacccd77eb3db52f7604127d67377
parksm13 <parksm13@students.ecu.edu>
2016-04-04 16:27:03 -0400

    Added link functionality to search results titles

--- resources/views/search/searchResultsWidget.blade.php
+++ resources/views/search/searchResultsWidget.blade.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Search Results</div>
+                <div class="panel-heading">Movie Search Results</div>
                 <div class="panel-body">
                     <table class="table table-hover">
 		        <tr>
@@ -13,7 +13,9 @@
                         </tr>
                         @foreach($movies as $movie)
 			    <tr>
-                                <td>{!! $movie->title !!}</td>
+                                <td>
+				  <a href="/movies/{!! $movie->id !!}">{!! $movie->title !!}</a>
+				</td>
                                 <td>{!! $movie->release_date !!}</td>
 				<td>{!! $movie->runtime !!} min</td>
 				<td>{!! $movie->genre !!}</td>
